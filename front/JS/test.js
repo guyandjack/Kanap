49ebb5a4e16e15f425167bb6435980fe3f65d7c1
guyandjack <guillaume-d@tutanota.com>
2022-05-15 20:34:31 +0200

    change api

--- front/JS/test.js
+++ front/JS/test.js
@@ -1,7 +1,12 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.end("Voilà la réponse du serveur !");
-});
-
-server.listen(process.env.PORT || 3000);
+fetch("https://www.prevision-meteo.ch/services/json/lat=46.259lng=5.235")
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (value) {
+    console.log(value);
+  })
+  .catch(function (err) {
+    alert("une erreur est survenu!");
+  });
