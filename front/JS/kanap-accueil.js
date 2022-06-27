1be94948042d5cce8176184b8c579365f2989f4b
guyandjack <guillaume-d@tutanota.com>
2022-06-27 20:48:56 +0200

    optimisation des fonctions affichage page accueil

--- front/JS/kanap-accueil.js
+++ front/JS/kanap-accueil.js
@@ -15,11 +15,12 @@ const sectionItems = document.getElementById("items");
 
 //****** déclaration des fonctions  *****************
 
-// crée un élément html "a", avec attribut href contenant les données pour la page produit, dans l' élément html "section id=items"
-function createLink(i) {
+
+
+// crée un élément html "a", avec attribut href contenant id du produit"
+function createLink(idProduct) {
   newLink = document.createElement("a");
-  let idValue = data[i]._id;
-  newLink.setAttribute("href", "./product.html?id=" + idValue);
+  newLink.setAttribute("href", "./product.html?id=" + idProduct);
   sectionItems.appendChild(newLink);
 }
 
@@ -27,67 +28,67 @@ function createLink(i) {
 function createArticle() {
   newArticle = document.createElement("article");
   newLink.appendChild(newArticle);
+  return newArticle
 }
 
 //crée un élément html "img" avec attribut src et alt dans l' élément "article"
-function createImg(i) {
+function createImg(imgUrl, altText) {
   newImg = document.createElement("img");
-  let altValue = data[i].altTxt;
-  let srcValue = data[i].imageUrl;
-  newImg.setAttribute("alt", altValue);
-  newImg.setAttribute("src", srcValue);
+  newImg.setAttribute("alt", altText);
+  newImg.setAttribute("src", imgUrl);
   newArticle.appendChild(newImg);
 }
 
 //crée un élément html "h3" avec son contenu textuel dans l' élément "article"
-function createH3(i) {
+function createH3(productName) {
   newH3 = document.createElement("h3");
   newH3.setAttribute("class", "productName");
-  let nameValue = data[i].name;
-  let newText = document.createTextNode(nameValue);
-  newH3.appendChild(newText);
+  newH3.innerText = productName;
   newArticle.appendChild(newH3);
 }
 
 //crée un élément html "p" avec son contenu textuel dans l' élément "article"
-function createP(i) {
+function createP(productDescription) {
   newP = document.createElement("p");
   newP.setAttribute("class", "productDescription");
-  let descriptionValue = data[i].description;
-  let newTextP = document.createTextNode(descriptionValue);
-  newP.appendChild(newTextP);
+  newP.innerText = productDescription;
   newArticle.appendChild(newP);
 }
 
-// fonction qui permet de créer un ensemble d' élément contenant les références produits
-function createBlockElements(compteur) {
-  let i = compteur;
-  createLink(i);
-  createArticle();
-  createH3(i);
-  createImg(i);
-  createP(i);
+// Affiche un seul produit
+function displayOneproduct(idProduct, productName, imgUrl, altText, productDescription) {
+  
+  createLink(idProduct);
+  let newArticle = createArticle();
+  createH3(productName);
+  createImg(imgUrl, altText);
+  createP(productDescription);
 }
 
 // Fonction principale qui regroupe l' ensemble des fonctions permetant la communication avec l' API et l' affichage des produits
 
 function displayProducts() {
+
   // Accession à l' API via son Url
   fetch(apiUrl)
+
     // Conversion au format Json des données recues de l' API
     .then(function (res) {
-      console.log(res);
-      res2 = res.json();
-      console.log(res2);
-      console.log(res);
-      return res2;
+      
+      return res.json()
     })
 
     // extrait les informations de chaque élèment de l' objet au format json
-    .then(function (value) {
-      data = value;
-      for (let i = 0; i < data.length; i++) {
-        createBlockElements(i);
+    .then(function (data) {
+      
+      for (let item of data) {
+
+        let idproduct = item._id;
+        let productName = item.name;
+        let imgUrl = item.imageUrl;
+        let altText = item.altTxt;
+        let productDescription = item.description;
+        displayOneproduct(idproduct, productName, imgUrl, altText, productDescription);
       }
     })
 
